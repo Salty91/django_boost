f282b4917c72daad6b0d29b15bc28a332cc757f2
Salty91 <prasolov.alexey91@gmail.com>
2020-07-31 21:08:21 +0300

    pytry

--- blog/views.py
+++ blog/views.py
@@ -1,25 +1,13 @@
 from django.shortcuts import render
 
-posts = [
-    {
-        'author': 'Salty91',
-        'title': 'First scratch',
-        'content': 'First post content',
-        'date_posted': 'July 22, 2020'
-    },
-    {
-        'author': 'Ashley Maddison',
-        'title': 'Scratch number 2',
-        'content': 'Second post content',
-        'date_posted': 'July 23, 2020'
-    }
-]
+from .models import Post
 
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
 def about(request):
     return render(request, 'blog/about.html', {'title': 'About'})
+
